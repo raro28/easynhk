ae2686ecdf18f95ca12dba4cd7880e5c7c427bcc
Hector Diaz <3kthor3adward@gmail.com>
2017-01-28 11:58:19 -0600

    list and get by id

--- index.php
+++ index.php
@@ -79,15 +79,38 @@ $slimDefinitions = [
     ContainerInterface::class => DI\get(Container::class),
 ];
 
+$dependencies = [
+    MongoDB\Client::class => DI\object(MongoDB\Client::class)->constructorParameter('uri', 'mongodb://localhost')
+];
+
 $containerBuilder = new \DI\ContainerBuilder ();
-$containerBuilder->addDefinitions($slimDefinitions);
+$containerBuilder->addDefinitions(array_merge($slimDefinitions, $dependencies));
 $container = $containerBuilder->build();
 
 $app = new App($container);
 
-$app->get('/', function(Psr\Http\Message\ResponseInterface $response) {
-    $response->getBody()->write('ok');
-    return $response;
+$app->get('/news/', function(\Psr\Http\Message\RequestInterface $request, Psr\Http\Message\ResponseInterface $response, MongoDB\Client $mongo) {
+    $page = $request->getQueryParam('page', 1);
+    $pageSize = $request->getQueryParam('pageSize', 10);
+    $skip = $pageSize * $page;
+    $desiredProperties = ['_id' => 0, 'title' => 1, 'news_id' => 1];
+
+    $response->getBody()->write(\GuzzleHttp\json_encode(iterator_to_array($mongo->newsdb->news->find([], ['skip' => $skip, 'limit' => $pageSize, 'projection' => $desiredProperties]))));
+
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/news/{id}', function(\Psr\Http\Message\RequestInterface $request,Psr\Http\Message\ResponseInterface $response, MongoDB\Client $mongo, $id) {
+    $desiredProperties = ['_id' => 0, 'title' => 1, 'news_id' => 1];
+    $result = $mongo->newsdb->news->findOne(['news_id' => $id],['projection'=>$desiredProperties]);
+    
+    if(!$result){
+        throw new Slim\Exception\NotFoundException($request,$response);
+    }
+
+    $response->getBody()->write(\GuzzleHttp\json_encode($result));
+
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
