6238151ad55943bb95264b5f0006010608921669
Hector Diaz <3kthor3adward@gmail.com>
2017-01-28 10:28:28 -0600

    store item

--- test.php
+++ test.php
@@ -23,53 +23,67 @@ $resources = [
     'news_easy_voice'=>function(\stdClass $newsItem,$baseUrl){return "$baseUrl/$newsItem->news_id/$newsItem->news_easy_voice_uri";}];
 
 function fileDownload($url){
-    $time = microtime();
-    $tmpFile = "/tmp/$time";
-    
-    $client = new \GuzzleHttp\Client();
-    $client->request('GET', $url, ['sink' => $tmpFile ]);
-    
-    
-    $data = file_get_contents($tmpFile);
-    $base64 = base64_encode($data);
-    
-    unlink($tmpFile);
+    try{
+        $time = microtime();
+        $tmpFile = "/tmp/$time";
+
+        $client = new \GuzzleHttp\Client();
+        $client->request('GET', $url, ['sink' => $tmpFile ]);
+
+
+        $data = file_get_contents($tmpFile);
+        $base64 = base64_encode($data);
+
+        unlink($tmpFile);
+    }catch(\GuzzleHttp\Exception\ClientException $ex){
+        $base64 = "";
+    }
     
     return $base64;
 }
 
 function extractArticle($url,$containerId,$removeTags=['rt','script']){
-    $contents = (new \GuzzleHttp\Client())->request('GET', $url)->getBody()->getContents();
-    $doc = DOMDocument::loadHTML($contents);
-    $article = $doc->getElementById($containerId);
-    foreach($removeTags as $removeTag){
-        $list = $article->getElementsByTagName($removeTag);
-        while ($list->length > 0) {
-            $p = $list->item(0);
-            $p->parentNode->removeChild($p);
-        }        
-    }
+    try{
+        $contents = (new \GuzzleHttp\Client())->request('GET', $url)->getBody()->getContents();
+        $doc = DOMDocument::loadHTML($contents);
+        $article = $doc->getElementById($containerId);
+        foreach($removeTags as $removeTag){
+            $list = $article->getElementsByTagName($removeTag);
+            while ($list->length > 0) {
+                $p = $list->item(0);
+                $p->parentNode->removeChild($p);
+            }        
+        }
 
-    return preg_replace('/\s+/', "", $article->textContent);
+        return $article;
+    }catch(\GuzzleHttp\Exception\ClientException $ex){
+        return null;
+    }
 }
+
+ $news = (new MongoDB\Client('mongodb://localhost'))->newsdb->news;
 
 $newsArray = \GuzzleHttp\json_decode($content)[0];
 foreach ($newsArray as $date=>$newsList){
     echo "$date\n";
     foreach($newsList as $newsItem){
+       if($news->count(['news_id'=>$newsItem->news_id])>0){ continue;}
+       
        echo "$newsItem->title\n";
        foreach($resources as $key=>$urlExtractor){
            if($newsItem->{"has_$key"}){
                $newsItem->{$key} = fileDownload(call_user_func($urlExtractor,$newsItem,$baseEasy));
            }
        }
+       
        if($newsItem->news_web_url != ''){
-           $newsItem->news_contents=extractArticle($newsItem->news_web_url,'main');
+           $article = extractArticle($newsItem->news_web_url,'main');    
+           $newsItem->news_contents= $article?preg_replace('/\s+/', "", $article->ownerDocument->saveHTML($article)):"not found";
        }
-       $newsItem->easy_contents=extractArticle("$baseEasy/$newsItem->news_id/$newsItem->news_id.html",'newsarticle');
-       echo print_r($newsItem);
-       break;
+       
+       $newsItem->easy_contents=preg_replace('/\s+/', "", extractArticle("$baseEasy/$newsItem->news_id/$newsItem->news_id.html",'newsarticle')->textContent);
+       
+       $news->insertOne((array)$newsItem);
     }
-    break;
 }
 
